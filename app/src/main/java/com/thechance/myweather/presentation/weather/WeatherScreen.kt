62d3854ef5cf5dabf7df242787611dcf9e92444d
Zyad <zyadhammad531@gmail.com>
2025-06-09 23:50:20 +0300

    add fonts

--- app/src/main/java/com/thechance/myweather/presentation/weather/WeatherScreen.kt
+++ app/src/main/java/com/thechance/myweather/presentation/weather/WeatherScreen.kt
@@ -1,7 +1,16 @@
 package com.thechance.myweather.presentation.weather
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.thechance.myweather.presentation.ui.theme.DayThemeColor
 import com.thechance.myweather.presentation.ui.theme.MyWeatherTheme
 import com.thechance.myweather.presentation.ui.theme.NightThemeColor
@@ -12,10 +21,27 @@ fun WeatherScreen(
 ) {
     val themeColor = state.themeColor
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(themeColor.backgroundGradientColor)
+            .statusBarsPadding()
+            .padding(
+                start = 12.dp,
+                top = 24.dp,
+                end = 12.dp,
+                bottom = 32.dp
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row {
+
+        }
+    }
 
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun PreviewWeatherScreen() {
     MyWeatherTheme {
@@ -27,7 +53,7 @@ private fun PreviewWeatherScreen() {
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun PreviewWeatherScreenNight() {
     MyWeatherTheme {
